0cf482c1f58f8c4b53a08cc65bbb9e8a10bf3a68
Terrence Reilly <terrencepreilly@gmail.com>
2016-01-09 08:01:08 -0500

    PEP8'ed

--- TTM.py
+++ TTM.py
@@ -1,25 +1,27 @@
 from datetime import datetime
 from datetime import timedelta
 
+
 class Entry(object):
-	def __init__(self, topic, start=None, end=None):
-		"""Create a new Entry object for use in a TimeTrackerManager."""
-		self.topic = topic
-		self.start = start
-		self.end = end
+
+    def __init__(self, topic, start=None, end=None):
+        """Create a new Entry object for use in a TimeTrackerManager."""
+        self.topic = topic
+        self.start = start
+        self.end = end
 
         def parse_line(self, line):
-                """Parse a line from a file for storage and use in data container
-                The line should be in the following format:
+                """Parse a line from a file for storage and use in data
+                container. The line should be in the following format:
                         <topic>\t<start>\t<end>
                 Where start and end are stored as Datetime Strings"""
                 if line.strip().lower() == None:
                         return None
                 topic, start, end = [a.strip() for a in line.split('\t')]
-                return topic, start, end	
+                return topic, start, end
 
         def parse_time(self, t):
-		"""Get datetime from a string, t."""
+                """Get datetime from a string, t."""
                 if t.lower().strip() == "none":
                         return None
                 date, time = t.split('T')
@@ -28,114 +30,121 @@ class Entry(object):
                 time[2], millis = time[2].split('.')
                 time.append(millis)
                 time = [int(a) for a in time]
-                return datetime(date[0], date[1], date[2], time[0], time[1], time[2], time[3])
+                return datetime(date[0], date[1], date[2],
+                                time[0], time[1], time[2], time[3])
 
-	def line_init(self, line):
-		"""Populate topic, start, and stop from the given line"""
-		pline = self.parse_line(line)
-		self.topic = pline[0]
-		self.start = None if pline[1] == "None" else self.parse_time(pline[1])
-		self.end = None if pline[2] == "None" else self.parse_time(pline[2])
+    def line_init(self, line):
+        """Populate topic, start, and stop from the given line"""
+        pline = self.parse_line(line)
+        self.topic = pline[0]
+        self.start = None if pline[1] == "None" else self.parse_time(pline[1])
+        self.end = None if pline[2] == "None" else self.parse_time(pline[2])
 
-	def __cmp__(self, other):
-		"""Compare self to another Entry on the basis of start time"""
-		if self.start == None:
-			return -1
-		elif other.start == None:
-			return 1
-		else:
-			return cmp(self.start, other.start)
+    def __cmp__(self, other):
+        """Compare self to another Entry on the basis of start time"""
+        if self.start == None:
+            return -1
+        elif other.start == None:
+            return 1
+        else:
+            return cmp(self.start, other.start)
 
-	def __str__(self):
-		start_str = "None"
-		if self.start != None:
-			start_str = datetime.isoformat(self.start)
-		end_str = "None"
-		if self.end != None:
-			end_str = datetime.isoformat(self.end)
-		return '\t'.join( [self.topic, start_str, end_str] )
+    def __str__(self):
 
-	def matches(self, topic):
-		return self.topic == topic
+        start_str = "None"
+        if self.start != None:
+            start_str = datetime.isoformat(self.start)
+        end_str = "None"
+        if self.end != None:
+            end_str = datetime.isoformat(self.end)
+        return '\t'.join( [self.topic, start_str, end_str] )
 
-	def delta(self):
-		return self.end - self.start
+    def matches(self, topic):
+
+        return self.topic == topic
+
+    def delta(self):
+
+        return self.end - self.start
+
 
 class TimeTrackerManager(object):
-	def __init__(self, filename=".defaultTracker.txt"):
-		self.filename = filename
-		self.data = list() # a list of tuples: (topic, start, end)
+    def __init__(self, filename=".defaultTracker.txt"):
+        self.filename = filename
+        self.data = list() # a list of tuples: (topic, start, end)
 
-	def load(self):
-		"""Load the database from filename"""
-		fin = None
-		try:
-			fin = open(self.filename, 'r')
-			lines = fin.readlines()
-			for line in lines:
-				entry = Entry('')
-				entry.line_init(line)
-				self.data.append( entry )
-		except IOError:
-			print "Data file does not exist. Creating new data file at " + self.filename
-		finally:
-			if fin != None:
-				fin.close()
+    def load(self):
+        """Load the database from filename"""
+        fin = None
+        try:
+            fin = open(self.filename, 'r')
+            lines = fin.readlines()
+            for line in lines:
+                entry = Entry('')
+                entry.line_init(line)
+                self.data.append( entry )
+        except IOError:
+            print("Data file does not exist. Creating new data file at " +
+                  self.filename)
+        finally:
+            if fin != None:
+                fin.close()
 
-	def save(self):
-		"""Write the current data to filename"""
-		fout = open(self.filename, 'w')
-		for datum in self.data:
-			fout.write(str(datum) + '\n')
-		fout.close()
+    def save(self):
+        """Write the current data to filename"""
+        fout = open(self.filename, 'w')
+        for datum in self.data:
+            fout.write(str(datum) + '\n')
+        fout.close()
 
-	def findRecentIndex(self, topic):
-		"""Super Slow Version"""
-		all_instances = list()
+    def findRecentIndex(self, topic):
+        """Super Slow Version"""
+        all_instances = list()
 
-		for d in range(len(self.data)):
-			if self.data[d].matches(topic):
-				all_instances.append((self.data[d], d))
+        for d in range(len(self.data)):
+            if self.data[d].matches(topic):
+                all_instances.append((self.data[d], d))
 
-		all_instances.sort()
-		if len(all_instances) == 0:
-			return None
-		if all_instances[-1][0].start == None or all_instances[-1][0].end != None:
-			print "No start time defined. Creating empty start time"
-			self.data.append( Entry(topic) )
-			return len(self.data) - 1
-		return all_instances[-1][1]
+        all_instances.sort()
+        if len(all_instances) == 0:
+            return None
+        if (all_instances[-1][0].start == None or
+            all_instances[-1][0].end != None):
+            print("No start time defined. Creating empty start time")
+            self.data.append( Entry(topic) )
+            return len(self.data) - 1
+        return all_instances[-1][1]
 
-	def start(self, topic):
-		"""Record start time for this topic."""
-		self.data.append( Entry(topic, datetime.now(), None) )
+    def start(self, topic):
+        """Record start time for this topic."""
+        self.data.append( Entry(topic, datetime.now(), None) )
 
-	def end(self, topic):
-		"""Record end time for this topic"""
-		i = self.findRecentIndex(topic)
-		if (i != None):
-			self.data[i].end = datetime.now()
+    def end(self, topic):
+        """Record end time for this topic"""
+        i = self.findRecentIndex(topic)
+        if (i != None):
+            self.data[i].end = datetime.now()
 
-	def filter_topic(self, t):
-		"""Filter etries by topic"""
-		f = lambda e : e.topic == t
-		return filter(f, self.data)
+    def filter_topic(self, t):
+        """Filter etries by topic"""
+        f = lambda e : e.topic == t
+        return filter(f, self.data)
 
-	def report_time_delta(self, td):
-		"""Reports total time delta in hours (rounded down)"""
-		hours = int(td.total_seconds() / 60 / 60)
-		return hours
+    def report_time_delta(self, td):
+        """Reports total time delta in hours (rounded down)"""
+        hours = int(td.total_seconds() / 60 / 60)
+        return hours
 
-	def report_time_delta_minutes(self, td):
-		"""Report total time delta in minutes (rounded down)"""
-		minutes = int(td.total_seconds() / 60)
-		return minutes
+    def report_time_delta_minutes(self, td):
+        """Report total time delta in minutes (rounded down)"""
+        minutes = int(td.total_seconds() / 60)
+        return minutes
 
-	def sum(self, topic):
-		"""Report the total sum of time, in timedelta"""
-		all_entries = self.filter_topic(topic)
-		tot = timedelta(0, 0, 0) 
-		for entry in all_entries:
-			tot += entry.delta()
-		return tot
+    def sum(self, topic):
+        """Report the total sum of time, in timedelta"""
+        all_entries = self.filter_topic(topic)
+        tot = timedelta(0, 0, 0)
+        for entry in all_entries:
+            tot += entry.delta()
+        return tot
 
--- TimeTracker.py
+++ TimeTracker.py
@@ -4,17 +4,25 @@ from TTM import *
 
 parser = argparse.ArgumentParser(description = "Track Time for Study/Work")
 
-parser.add_argument('-f', nargs = '?', help = "The filename for the data storage.")
+parser.add_argument('-f', nargs = '?',
+                    help = "The filename for the data storage.")
 
-parser.add_argument('-l', nargs = '?', help = "The label associated wit this time.")
+parser.add_argument('-l', nargs = '?',
+                    help = "The label associated wit this time.")
 
-parser.add_argument('-b', action = "store_true", help = "Set flag to begin tracking")
+parser.add_argument('-b', action = "store_true",
+                    help = "Set flag to begin tracking")
 
-parser.add_argument("-e", action = "store_true", help = "Set flag to stop tracking")
+parser.add_argument("-e", action = "store_true",
+                    help = "Set flag to stop tracking")
 
-parser.add_argument("-s", nargs = "?", help = "Return the sum total of time spent on the given subject, in hours (rounded down)")
+parser.add_argument("-s", nargs = "?",
+                    help = """Return the sum total of time spent on the given
+                              subject, in hours (rounded down)""")
 
-parser.add_argument("-m", action = "store_true", help = "For sum total report, report in minutes (rounded down)")
+parser.add_argument("-m", action = "store_true",
+                    help = """For sum total report, report in minutes (rounded
+                               down)""")
 
 args = parser.parse_args()
 
@@ -29,20 +37,20 @@ label = args.l if args.l else None
 #----------------BEGIN TRACKING-----------------------------------#
 # create a new data entry
 if args.b:
-	ttm.start(label)
+    ttm.start(label)
 
 #----------------STOP TRACKING------------------------------------#
-# find the most recent data entry of type label, if no end, add it otherwise 
+# find the most recent data entry of type label, if no end, add it otherwise
 # throw error
 if args.e:
-	ttm.end(label)
+    ttm.end(label)
 
 ttm.save()
 
 #----------------GET SUM TIME SPENT-------------------------------#
 if args.s:
-	print "Time spend on", args.s, ": ",
-	if args.m:
-		print ttm.report_time_delta_minutes(ttm.sum(args.s))
-	else:
-		print ttm.report_time_delta(ttm.sum(args.s))
+    print("Time spend on", args.s, ": ",)
+    if args.m:
+        print(ttm.report_time_delta_minutes(ttm.sum(args.s)))
+    else:
+        print(ttm.report_time_delta(ttm.sum(args.s)))
